39ea7f67bbcc1087c08157d79ae5d9e880d7f5c9
MarkelOnaindia <markel.onaindia@ikasle.egibide.org>
2023-11-08 10:06:27 +0100

    organisation and nav

---
+++ web/views/index.view.php
@@ -0,0 +1,15 @@
+<?php require "views/partials/header.php" ?>
+
+    <div class="navbar">
+        <a href="#telefonia">Telefonía e Informática</a>
+        <a href="#ocio">Ocio</a>
+        <a href="#estetica">Estética y Salud</a>
+        <a href="#librerias">Librerías y Papelerías</a>
+        <a href="#hogar">Hogar</a>
+        <a href="#servicios">Servicios</a>
+        <a href="#moda">Moda y Complementos</a>
+        <a href="#hosteleria">Hostelería</a>
+        <a href="#alimentacion">Alimentación</a>
+    </div>
+
+<?php require "views/partials/footer.php" ?>    

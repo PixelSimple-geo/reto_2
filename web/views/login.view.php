9682a2bf92d085d33843204df42dafb8629329cf
MarkelOnaindia <126677198+MarkelOnaindia@users.noreply.github.com>
2023-11-09 12:07:58 +0100

    maps and conection

---
+++ web/views/login.view.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Login y Registro</title>
+ 
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;500;600&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="../css/reset.css">
+    <link rel="stylesheet" href="../css/login.css">
+</head>
+<body>
+    <div class="background">
+        <div class="shape"></div>
+        <div class="shape"></div>
+    </div>
+    <form>
+        <h3 id="form-title">Login</h3>
+
+        <label for="usuario">Usuario</label>
+        <input type="text" id="usuario">
+
+        <label for="password">Contraseña</label>
+        <input type="password" id="password">
+
+        <button id="submitButton">Iniciar Sesión</button>
+        <div class="social">
+          <div class="enlace">¿Has olvidado tu contraseña?</div>
+        </div>
+    </form>
+
+    <script>
+        const formTitle = document.getElementById('form-title');
+        const submitButton = document.getElementById('submitButton');
+
+        function toggleForm() {
+            if (formTitle.textContent === 'Login') {
+                formTitle.textContent = 'Registro';
+                submitButton.textContent = 'Registrarse';
+            } else {
+                formTitle.textContent = 'Login';
+                submitButton.textContent = 'Iniciar Sesión';
+            }
+        }
+
+        formTitle.addEventListener('click', toggleForm);
+    </script>
+</body>
+</html>

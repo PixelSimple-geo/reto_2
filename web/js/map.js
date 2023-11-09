db4677ab2d66b773d3c4221933fc728fb981f42e
MarkelOnaindia <126677198+MarkelOnaindia@users.noreply.github.com>
2023-11-09 11:56:39 +0100

    maps and conection

---
+++ web/js/map.js
@@ -0,0 +1,14 @@
+function initMap() {
+    var center = { lat: 42.84998, lng: -2.67268}; 
+
+    var map = new google.maps.Map(document.getElementById('map'), {
+        center: center,
+        zoom: 12 
+    });
+
+    var marker = new google.maps.Marker({
+        position: center,
+        map: map,
+        title: 'Vitoria-Gasteiz'
+    });
+}

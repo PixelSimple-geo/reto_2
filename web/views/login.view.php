d94d157a5dfb0cf3f3fb3d5b23117a075b7e8161
MarkelOnaindia <markel.onaindia@ikasle.egibide.org>
2023-11-13 09:47:32 +0100

    login and register validations

--- web/views/login.view.php
+++ web/views/login.view.php
@@ -1,48 +1,32 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <title>Login y Registro</title>
- 
+    <title>Login</title>
+    
+    <!--CSS-->
     <link rel="stylesheet" href="../statics/css/reset.css">
     <link rel="stylesheet" href="../statics/css/style.css">
 
+    <!--JS-->
+    <script src="../statics/js/logreg.js" defer></script>
+
 </head>
 <body>
-    <div>
-        <button id="login">Inicior Sesion</button>
-        <button id="register">Registrarse</button>
+    
+    <form id="log_form" action="../index.php" method="POST" >
 
-        <form id="log_form" action="../index.php" method="POST">
+        <label for="usuario">Usuario</label>
+        <input type="text" id="usuario" required>
 
-            <label for="usuario">Usuario</label>
-            <input type="text" id="usuario">
+        <label for="password">Contraseña</label>
+        <input type="password" id="password" required>
 
-            <label for="password">Contraseña</label>
-            <input type="password" id="password">
+        <button id="submitLog">Iniciar Sesión</button>
 
-            <button id="submitLog">Iniciar Sesión</button>
-            <a href="#">¿Has olvidado tu contraseña?</a>
+    </form>
 
-        </form>
-
-        <form id="reg_form" action="../index.php" method="POST">
-
-            <label for="usuario">Usuario</label>
-            <input type="text" id="usuario">
-
-            <label for="password">Contraseña</label>
-            <input type="password" id="password">
-
-            <label for="conpassword">Confirmar Contraseña</label>
-            <input type="password" id="conpassword">
-
-            <button id="submitReg">Registrarse</button>
-
-            <input id="terminos" type="checkbox">
-            <label for="terminos">Acepto los <a href="#">terminos y condiciones</a></label>
-
-        </form>
-    </div>
+    <a href="#">¿Has olvidado tu contraseña?</a>
+    <a href="register.view.php">¿No tienes cuenta? Registrate</a>
     
 </body>
 </html>
---
+++ web/views/register.view.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Registro</title>
+ 
+    <!--CSS-->
+    <link rel="stylesheet" href="../statics/css/reset.css">
+    <link rel="stylesheet" href="../statics/css/style.css">
+
+    <!--JS-->
+    <script src="../statics/js/logreg.js" defer></script>
+
+</head>
+<body>
+
+    <form id="reg_form" action="../index.php" method="POST">
+
+        <label for="usuario">Usuario</label>
+        <input type="text" id="usuario" required>
+
+        <label for="password">Contraseña</label>
+        <input type="password" id="password" required>
+
+        <label for="conpassword">Confirmar Contraseña</label>
+        <input type="password" id="conpassword" required>
+
+        <label for="email">Email</label>
+        <input type="email" id="email" required>
+
+        <button id="submitReg">Registrarse</button>
+
+        <input id="terminos" type="checkbox" required>
+        <label for="terminos">*Acepto los <a href="#">terminos y condiciones</a></label>
+
+    </form>
+    
+    <a href="login.view.php">¿Ya tienes cuenta? Inicia Sesion</a>
+</body>
+</html>

759936a9b935b891aa37339a4414ca5f6a0f1e66
MarkelOnaindia <markel.onaindia@ikasle.egibide.org>
2023-11-17 13:49:26 +0100

    index solucionado

--- web/controllers/mainController.php
+++ web/controllers/mainController.php
@@ -1,8 +1,11 @@
 <?php
 
 function index() :void {
+    require_once $_SERVER['DOCUMENT_ROOT'] . "/models/advertsDB.php";
+
     try {
         $userAccount = getUserAccountFromSession();
+        $adverts = getAllAdverts();
     } catch (Exception $exception) {
         $userAccount = null;
     }

ce9addf7bbcdbde5ac95382ea40b0eaac5b58212
MarkelOnaindia <markel.onaindia@ikasle.egibide.org>
2023-11-19 21:45:54 +0100

    article separated by pages

--- web/views/articlesView/articleClient.view.php
+++ web/views/articlesView/articleClient.view.php
@@ -4,6 +4,16 @@
 <head>
     <?php require $_SERVER['DOCUMENT_ROOT'] . "/views/partials/head.php" ?>
     <title>Noticias | Reto 2</title>
+    <style>
+        .pagination {
+            display: flex;
+            list-style: none;
+        }
+
+        .pagination li {
+            margin-right: 5px;
+        }
+    </style>
 </head>
 
 <body>
@@ -11,13 +21,33 @@
 
     <div class="articles">
         <?php
-        if ($articles) {
-            foreach ($articles as $article) {
+        $itemsPerPage = 6;
+        $totalItems = count($articles);
+
+        $currentPage = isset($_GET['page']) ? max(1, intval($_GET['page'])) : 1;
+        $offset = ($currentPage - 1) * $itemsPerPage;
+
+        $pagedArticles = array_slice($articles, $offset, $itemsPerPage);
+
+        if (isset($pagedArticles) && !empty($pagedArticles)) {
+            foreach ($pagedArticles as $article) {
                 echo '<div class="article">';
-                    echo '<h2>' . htmlspecialchars($article['title']) . '</h2>';
-                    echo '<p>' . htmlspecialchars($article['description']) . '</p>';
-                    echo '<p>' . htmlspecialchars($article['created_date']) . '</p>';
+                echo '<h2>' . htmlspecialchars($article['title']) . '</h2>';
+                echo '<p>' . htmlspecialchars($article['description']) . '</p>';
+                echo '<p>' . htmlspecialchars($article['created_date']) . '</p>';
                 echo '</div>';
+            }
+
+            // Paginación
+            $totalPages = ceil($totalItems / $itemsPerPage);
+
+            if ($totalPages > 1) {
+                echo '<ul class="paginas">';
+                for ($i = 1; $i <= $totalPages; $i++) {
+                    $class = ($i == $currentPage) ? 'current' : '';
+                    echo '<li><a href="?page=' . $i . '" class="' . $class . '">' . $i . '</a></li>';
+                }
+                echo '</ul>';
             }
         } else {
             echo '<p>No se encontraron noticias.</p>';
--- web/views/articlesView/articles.view.php
+++ web/views/articlesView/articles.view.php
@@ -13,19 +13,38 @@
             <h2>Mis Articulos</h2>
             <div class="contents">
                 <?php
-                    if (isset($articulos)) {
-                        foreach ($articulos as $articulo) {
+                    $itemsPerPage = 6;
+                    $totalItems = count($articulos);
+
+                    $currentPage = isset($_GET['page']) ? max(1, intval($_GET['page'])) : 1;
+                    $offset = ($currentPage - 1) * $itemsPerPage;
+
+                    $pagedArticulos = array_slice($articulos, $offset, $itemsPerPage);
+
+                    if (isset($pagedArticulos) && !empty($pagedArticulos)) {
+                        foreach ($pagedArticulos as $articulo) {
                             echo '<div>';
                             echo '  <h3>' . $articulo['title'] . '</h3>';
                             echo '  <p>Dirección: ' . $articulo['description'] . '</p>';
                             echo '  <p>Categoría: ' . $articulo['category'] . '</p>';
                             echo '  <a href="/articles/' . $articulo['articleId'] . '">Editar Articulo</a>';
-                            //TODO terminar el enlace de eliminacion
-                            echo '  <a href="/.php?id=' . $articulo['articleId'] . '" onclick="return confirm(\'¿Estás seguro de que deseas eliminar este articulo?\')" class="eliminar">Eliminar Articulo</a>';
+                            // TODO: Terminar el enlace de eliminación
+                            echo '  <a href="/eliminar-articulo.php?id=' . $articulo['articleId'] . '" onclick="return confirm(\'¿Estás seguro de que deseas eliminar este artículo?\')" class="eliminar">Eliminar Articulo</a>';
                             echo '</div>';
                         }
+
+                        $totalPages = ceil($totalItems / $itemsPerPage);
+
+                        if ($totalPages > 1) {
+                            echo '<ul class="paginas">';
+                            for ($i = 1; $i <= $totalPages; $i++) {
+                                $class = ($i == $currentPage) ? 'current' : '';
+                                echo '<li><a href="?page=' . $i . '" class="' . $class . '">' . $i . '</a></li>';
+                            }
+                            echo '</ul>';
+                        }
                     } else {
-                        echo '<p>No tienes articulos registrados.</p>';
+                        echo '<p>No tienes artículos registrados.</p>';
                     }
                 ?>
             </div>

5bb50376967897d60866dfe6c8749b8468854b64
pirate <jorge.egea@ikasle.egibide.org>
2023-11-22 09:54:49 +0100

    RequestDispatcher improved

--- web/handlers/requestDispatcher.php
+++ web/handlers/requestDispatcher.php
@@ -3,10 +3,6 @@
 require_once $_SERVER["DOCUMENT_ROOT"] . "/models/driverManager.php";
 require_once $_SERVER["DOCUMENT_ROOT"] . "/handlers/sessionHandler.php";
 
-function matchURI($URI) :bool {
-    return str_starts_with($_SERVER['REQUEST_URI'], $URI);
-}
-
 function validateRequiredParameters(array $parameters, $source = "POST"): void {
     $source = strtoupper($source);
     $requestData = ($source === "GET") ? $_GET : $_POST;
@@ -18,9 +14,6 @@ function validateRequiredParameters(array $parameters, $source = "POST"): void {
 }
 
 
-
-
-$requestMethod = $_SERVER["REQUEST_METHOD"];
 startSession();
 try {
     $userAccount = getUserAccountFromSession();
@@ -30,134 +23,247 @@ try {
 
 $url = $_SERVER["REQUEST_URI"];
 $parsedUrl = parse_url($url);
-$pathSegments = explode("/", $parsedUrl["path"]);
-array_shift($pathSegments);
-print_r($pathSegments);
+$requestedRoute = explode("/", $parsedUrl["path"]);
+array_shift($requestedRoute);
 
+$routeMapping = [
+    "login" => [
+        "controller" => "accountController",
+        "methods" => [
+            "GET" => "getLogin",
+            "POST" => "postLogin"
+        ]
+    ],
+    "logout" => [
+        "controller" => "mainController",
+        "methods" => [
+            "GET" => "logout"
+        ]
+    ],
+    "signIn" => [
+        "controller" => "accountController",
+        "methods" => [
+            "GET" => "getSignIn",
+            "POST" => "postSignIn"
+        ]
+    ],
+    "history" => [
+        "controller" => "mainController",
+        "methods" => [
+            "GET" => "history"
+        ]
+    ],
+    "contact" => [
+        "controller" => "mainController",
+        "methods" => [
+            "GET" => "contact"
+        ]
+    ],
+    "index" => [
+        "controller" => "mainController",
+        "methods" => [
+            "GET" => "index"
+        ]
+    ],
+    "account" => [
+        "controller" => "accountController",
+        "methods" => [
+            "GET" => "getProfile",
+            "POST" => "postProfile",
+            "DELETE" => "deleteUserAccount"
+        ]
+    ],
+    "businesses" => [
+        "controller" => "businessesController",
+        "business" => [
+            "methods" => [
+                "GET" => "getBusinessPage"
+            ]
+        ],
+        "all" => [
+            "methods" => [
+                "GET" => "getBusinesses"
+            ]
+        ],
+        "crud" => [
+            "all" => [
+                "methods" => [
+                    "GET" => "getBusinessesCrudReadAll"
+                ]
+            ],
+            "business" => [
+                "methods" => [
+                    "GET" => "getBusinessesCrudRead"
+                ]
+            ],
+            "add" => [
+                "methods" => [
+                    "GET" => "getBusinessesCrudAdd",
+                    "POST" => "postBusinessesCrudAdd"
+                ]
+            ],
+            "edit" => [
+                "methods" => [
+                    "GET" => "getBusinessesCrudEdit",
+                    "POST" => "postBusinessesCrudEdit"
+                ]
+            ],
+            "delete" => [
+                "methods" => [
+                    "GET" => "getBusinessesCrudDelete"
+                ]
+            ]
+        ],
+        "advertCategory" => [
+            "add" => [
+                "methods" => [
+                    "POST" => "postBusinessesAdvertCategoryCrudAdd"
+                ]
+            ],
+            "delete" => [
+                "methods" => [
+                    "GET" => "deleteBusinessesAdvertCategoryCrudDelete"
+                ]
+            ]
+        ]
+    ],
+    "adverts" => [
+        "controller" => "advertsController",
+        "advert" => [],
+        "all" => [],
+        "crud" => [
+            "add" => [
+                "methods" => [
+                    "GET" => "getAdvertsCrudAdd",
+                    "POST" => "postAdvertsCrudAdd"
+                ]
+            ],
+            "edit" => [
+                "methods" => [
+                    "GET" => "getAdvertsCrudEdit",
+                    "POST" => "postAdvertsCrudEdit"
+                ]
+            ],
+            "delete" => [
+                "methods" => [
+                    "GET" => "getAdvertsCrudDelete"
+                ]
+            ]
+        ]
+    ],
+    "articles" => [
+        "controller" => "articlesController",
+        "article" => [
+            "methods" => [
+                "GET" => "getArticleById"
+            ]
+        ],
+        "all" => [
+            "methods" => [
+                "GET" => "getArticles"
+            ]
+        ],
+        "crud" => [
+            "all" => [
+                "methods" => [
+                    "GET" => "getArticlesCrudReadAll"
+                ]
+            ],
+            "add" => [
+                "methods" => [
+                    "GET" => "getArticlesCrudAdd",
+                    "POST" => "postArticlesCrudAdd"
+                ]
+            ],
+            "edit" => [
+                "methods" => [
+                    "GET" => "getArticlesCrudEdit",
+                    "POST" => "postArticlesCrudEdit"
+                ]
+            ],
+            "delete" => [
+                "methods" => [
+                    "GET" => "getArticlesCrudDelete"
+                ]
+            ]
+        ]
+    ],
+    "reviews" => [
+        "controller" => "reviewsController",
+        "crud" => [
+            "add" => [
+                "methods" => [
+                    "POST" => "postReviewCrudAdd"
+                ]
+            ]
+        ]
+    ],
+    "likes" => [
+        "controller" => "reviewsController",
+        "crud" => [
+            "add" => [
+                "methods" => [
+                    "POST" => "postReviewLikeCrudAdd"
+                ]
+            ]
+        ]
+    ],
+    "products" => [
+        "controller" => "advertsController",
+        "methods" => [
+            "GET" => "getAdverts"
+        ]
+    ],
+    "commentaries" => [
+        "controller" => "commentariesController",
+        "crud" => [
+            "add" => [
+                "methods" => [
+                    "POST" => "postCommentaryCrudAdd"
+                ]
+            ]
+        ]
+    ],
+    "commentariesLikes" => [
+        "controller" => "commentariesController",
+        "crud" => [
+            "add" => [
+                "methods" => [
+                    "POST" => "postCommentaryLikeCrudAdd"
+                ]
+            ]
+        ]
+    ]
+];
 
-if (matchURI("/login")) {
-    require_once $_SERVER['DOCUMENT_ROOT'] . "/controllers/accountController.php";
-    switch ($requestMethod) {
-        case "GET":
-            getLogin();
-            break;
-        case "POST":
-            postLogin();
-            break;
-    }
-} else if (matchURI("/logout")) {
-    require_once $_SERVER['DOCUMENT_ROOT'] . "/controllers/mainController.php";
-    logout();
-} else if (matchURI("/signIn")) {
-    require_once $_SERVER['DOCUMENT_ROOT'] . "/controllers/accountController.php";
-    switch ($requestMethod) {
-        case "GET":
-            getSignIn();
-            break;
-        case "POST":
-            postSignIn();
-            break;
-    }
-} else if (matchURI("/history")) {
-    require_once $_SERVER['DOCUMENT_ROOT'] . "/controllers/mainController.php";
-    if ($requestMethod === "GET") history();
-} else if (matchURI("/contact")) {
-    require_once $_SERVER['DOCUMENT_ROOT'] . "/controllers/mainController.php";
-    if ($requestMethod === "GET") contact();
-} else if (matchURI("/index")){
-    require_once $_SERVER['DOCUMENT_ROOT'] . "/controllers/mainController.php";
-    index();
-} else if (matchURI("/account")){
-    require_once $_SERVER['DOCUMENT_ROOT'] . "/controllers/accountController.php";
-    switch ($requestMethod) {
-        case "GET":
-            getProfile();
-            break;
-        case "POST":
-            postProfile();
-            break;
-        case "DELETE":
-            deleteUserAccount();
-            break;
-    }
-} else if (matchURI("/businesses")) {
-    require_once $_SERVER['DOCUMENT_ROOT'] . "/controllers/businessesController.php";
-    if (matchURI("/businesses/business")) {
-        if ($requestMethod === "GET") getBusinessPage();
-    } else if (matchURI("/businesses/all")) {
-        if ($requestMethod === "GET") getBusinesses();
-    } else if (matchURI("/businesses/crud")) {
-        if (matchURI("/businesses/crud/all")) {
-            if ($requestMethod === "GET") getBusinessesCrudReadAll();
-        } else if (matchURI("/businesses/crud/business")) {
-            if ($requestMethod === "GET") getBusinessesCrudRead();
-        } else if (matchURI("/businesses/crud/add")) {
-            if ($requestMethod === "GET") getBusinessesCrudAdd();
-            else if ($requestMethod === "POST") postBusinessesCrudAdd();
-        } else if (matchURI("/businesses/crud/edit")) {
-            if ($requestMethod === "GET") getBusinessesCrudEdit();
-            else if ($requestMethod === "POST") postBusinessesCrudEdit();
-        } else if (matchURI("/businesses/crud/delete")) {
-            if ($requestMethod === "GET") getBusinessesCrudDelete();
-        }
-    } else if (matchURI("/businesses/advertCategory")) {
-        if(matchURI("/businesses/advertCategory/add")) postBusinessesAdvertCategoryCrudAdd();
-        else if(matchURI("/businesses/advertCategory/delete")) deleteBusinessesAdvertCategoryCrudDelete();
-    }
-} else if (matchURI("/adverts")) {
-    require_once $_SERVER['DOCUMENT_ROOT'] . "/controllers/advertsController.php";
-    if (matchURI("/adverts/advert")) {
-        // For client
-    } else if (matchURI("/adverts/all")) {
+$requestMethod = $_SERVER["REQUEST_METHOD"];
+$isInvalidRequest = false;
+$controller = null;
+$delegateFunction = null;
+$currentRoute = $routeMapping;
 
-    } else if (matchURI("/adverts/crud")) {
-        if (matchURI("/adverts/crud/add")) {
-            if($requestMethod === "GET") getAdvertsCrudAdd();
-            else if ($requestMethod === "POST") postAdvertsCrudAdd();
-        } else if (matchURI("/adverts/crud/edit")) {
-            if ($requestMethod === "GET") getAdvertsCrudEdit();
-            else if ($requestMethod === "POST") postAdvertsCrudEdit();
-        } else if (matchURI("/adverts/crud/delete")) {
-            if ($requestMethod === "GET") getAdvertsCrudDelete();
-        }
+foreach ($requestedRoute as $index => $routeFragment) {
+    if (!array_key_exists($routeFragment, $currentRoute)) {
+        $isInvalidRequest = true;
+        break;
     }
-} else if (matchURI("/articles")) {
-    require_once $_SERVER['DOCUMENT_ROOT'] . "/controllers/articlesController.php";
-    if (matchURI("/articles/article")) {
-        if ($requestMethod === "GET") getArticleById();
-    } else if (matchURI("/articles/all")) {
-        if ($requestMethod === "GET") getArticles();
-    } else if (matchURI("/articles/crud")) {
-        if (matchURI("/articles/crud/all")) {
-            if ($requestMethod === "GET") getArticlesCrudReadAll();
-        } else if (matchURI("/articles/crud/add")) {
-            if ($requestMethod === "GET") getArticlesCrudAdd();
-            else if ($requestMethod === "POST") postArticlesCrudAdd();
-        } else if (matchURI("/articles/crud/edit")) {
-            if ($requestMethod === "GET") getArticlesCrudEdit();
-            else if ($requestMethod === "POST") postArticlesCrudEdit();
-        } else if (matchURI("/articles/crud/delete")) {
-            if ($requestMethod === "GET") getArticlesCrudDelete();
-        }
+
+    $currentRoute = $currentRoute[$routeFragment];
+
+    if (array_key_exists("controller", $currentRoute)) {
+        $controller = $currentRoute["controller"] . ".php";
     }
-} else if (matchURI("/reviews/crud")){
-    require_once $_SERVER['DOCUMENT_ROOT'] . "/controllers/reviewsController.php";
-    if (matchURI("/reviews/crud/add")) postReviewCrudAdd();
-} else if (matchURI("/likes/crud")) {
-    require_once $_SERVER['DOCUMENT_ROOT'] . "/controllers/reviewsController.php";
-    if (matchURI("/likes/crud/add")) postReviewLikeCrudAdd();
-} else if (matchURI("/products")){
-    require_once $_SERVER['DOCUMENT_ROOT'] . "/controllers/advertsController.php";
-    getAdverts();
-} else if (matchURI("/commentaries/crud")) {
-    require_once $_SERVER['DOCUMENT_ROOT'] . "/controllers/commentariesController.php";
-    if (matchURI("/commentaries/crud/add"))
-        if ($requestMethod === "POST") postCommentaryCrudAdd();
-} else if (matchURI("/commentariesLikes/crud/add")) {
-    require_once $_SERVER['DOCUMENT_ROOT'] . "/controllers/commentariesController.php";
-    if (matchURI("/commentariesLikes/crud/add")) {
-        if ($requestMethod === "POST") postCommentaryLikeCrudAdd();
+
+    if ($index === count($requestedRoute) - 1 && array_key_exists("methods", $currentRoute) &&
+        array_key_exists($requestMethod, $currentRoute["methods"])) {
+        $delegateFunction = $currentRoute["methods"][$requestMethod];
     }
 }
 
+if ($isInvalidRequest || $controller === null || $delegateFunction === null) {
+    include_once $_SERVER["DOCUMENT_ROOT"] . "/views/error_404.view.php";
+    die();
+}
 
+require_once $_SERVER['DOCUMENT_ROOT'] . "/controllers/$controller";
+call_user_func($delegateFunction);

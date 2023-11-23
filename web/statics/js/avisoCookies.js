faa46eff66ae972924ca40e76db43b1324a80441
MarkelOnaindia <markel.onaindia@ikasle.egibide.org>
2023-11-23 09:54:04 +0100

    cookies hecho

---
+++ web/statics/js/avisoCookies.js
@@ -0,0 +1,48 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const botonAceptarCookies = document.getElementById('btn-aceptar-cookies');
+    const avisoCookies = document.getElementById('aviso-cookies');
+    const fondoAvisoCookies = document.getElementById('fondo-aviso-cookies');
+
+    dataLayer = [];
+
+    function setCookie(name, value, days) {
+        const date = new Date();
+        date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
+        const expires = "expires=" + date.toUTCString();
+        document.cookie = name + "=" + value + ";" + expires + ";path=/";
+    }
+
+    function getCookie(name) {
+        const cname = name + "=";
+        const decodedCookie = decodeURIComponent(document.cookie);
+        const ca = decodedCookie.split(';');
+        for (let i = 0; i < ca.length; i++) {
+            let c = ca[i];
+            while (c.charAt(0) === ' ') {
+                c = c.substring(1);
+            }
+            if (c.indexOf(cname) === 0) {
+                return c.substring(cname.length, c.length);
+            }
+        }
+        return "";
+    }
+
+    if (!getCookie('cookies-aceptadas')) {
+        avisoCookies.classList.add('activo');
+        fondoAvisoCookies.classList.add('activo');
+		avisoCookies.style.cursor = "pointer";
+		fondoAvisoCookies.style.cursor = "not-allowed";
+    } else {
+        dataLayer.push({ 'event': 'cookies-aceptadas' });
+    }
+
+    botonAceptarCookies.addEventListener('click', () => {
+        avisoCookies.classList.remove('activo');
+        fondoAvisoCookies.classList.remove('activo');
+
+        setCookie('cookies-aceptadas', 'true', 30);
+
+        dataLayer.push({ 'event': 'cookies-aceptadas' });
+    });
+});

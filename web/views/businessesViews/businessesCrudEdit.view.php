4ca32e14047aded3f58f9b1603506c95d612edb8
MarkelOnaindia <markel.onaindia@ikasle.egibide.org>
2023-11-24 13:58:54 +0100

    corregido edit business

--- web/views/businessesViews/businessesCrudEdit.view.php
+++ web/views/businessesViews/businessesCrudEdit.view.php
@@ -45,33 +45,64 @@
                     <?php endif; ?>
 
                     <fieldset>
-                        <legend>Contacto</legend>
-                        <div id="contactsContainer">
-                            <?php foreach ($business['contacts'] as $contact): ?>
+                    <legend>Contacto</legend>
+                    <button type="button" onclick="agregarContacto()">+</button>
+                    <div id="contactsContainer">
+                        <?php foreach ($business['contacts'] as $contact): ?>
+                            <div>
+                                <label for='type_<?= $contact['contactId'] ?>'>Tipo de contacto</label>
+                                <input id='type_<?= $contact['contactId'] ?>' value='<?= $contact['type'] ?>' name='contact_type[]'
+                                    pattern="^.{1,100}$" title="Ingresa entre 1 y 100 caracteres para el tipo de contacto">
+                                
+                                <label for='value_<?= $contact['contactId'] ?>'>Dirección de medio</label>
+                                <input id='value_<?= $contact['contactId'] ?>' value='<?= $contact['value'] ?>' name='contact_value[]'
+                                    pattern="^.{1,255}$" title="Ingresa entre 1 y 255 caracteres para la dirección de medio">
+                                
+                                <button data-script-delete-con-dir>Eliminar</button>
+                            </div>
+                        <?php endforeach; ?>
+                        <?php if (!empty($contacts)): ?>
+                            <?php foreach ($contacts as $index => $value): ?>
+                            <div>
+                                <label for=<?=$index . "_type"?>>Tipo de contacto</label>
+                                <input id=<?=$index . "_type"?> value='<?=$value["type"]?>' name="contact_type[]">
+                                <label for=<?=$index . "_value"?>>Dirección de medio</label>
+                                <input id=<?=$index . "_value"?> value='<?=$value["value"]?>' name="contact_value[]">
+                            </div>
+                            <?php endforeach; ?>
+                        <?php endif; ?>
+                    </div>
+                </fieldset>
+
+                <fieldset>
+                    <legend>Dirección</legend>
+                    <button type="button" onclick="agregarDireccion()">+</button>
+                    <div id="addressesContainer">
+                        <?php foreach ($business['addresses'] as $address): ?>
+                            <div>
+                                <label for='address_<?= $address['addressId'] ?>'>Dirección</label>
+                                <input id='address_<?= $address['addressId'] ?>' value='<?= $address['address'] ?>' name='addresses[]'
+                                    pattern="^.{1,100}$" title="Ingresa entre 1 y 100 caracteres para la dirección">
+                                
+                                <label for='postal_code_<?= $address['addressId'] ?>'>Código Postal</label>
+                                <input id='postal_code_<?= $address['addressId'] ?>' value='<?= $address['postalCode'] ?>' name='postal_codes[]'
+                                    pattern="[0-9]{5}" title="Ingresa un número de 5 dígitos para el código postal">
+                                
+                                <button data-script-delete-con-dir>Eliminar</button>
+                            </div>
+                        <?php endforeach; ?>
+                        <?php if (!empty($addresses)): ?>
+                            <?php foreach ($addresses as $index => $value): ?>
                                 <div>
-                                    <label for='type_<?= $contact['contactId'] ?>'>Tipo de contacto</label>
-                                    <input id='type_<?= $contact['contactId'] ?>' value='<?= $contact['type'] ?>' name='contact_type[]'
-                                        pattern="^.{1,100}$" title="Ingresa entre 1 y 100 caracteres para el tipo de contacto">
-                                    
-                                    <label for='value_<?= $contact['contactId'] ?>'>Dirección de medio</label>
-                                    <input id='value_<?= $contact['contactId'] ?>' value='<?= $contact['value'] ?>' name='contact_value[]'
-                                        pattern="^.{1,255}$" title="Ingresa entre 1 y 255 caracteres para la dirección de medio">
-                                    
-                                    <button data-script-delete-con-dir>Eliminar</button>
+                                    <label for=<?=$index . "_address"?>>Dirección</label>
+                                    <input id=<?=$index . "_address"?> value='<?=$value["address"]?>' name="addresses[]">
+                                    <label for=<?=$index . "_postal_code"?>>Código postal de medio</label>
+                                    <input id=<?=$index . "_postal_code"?> value='<?=$value["postalCode"]?>' name="postal_codes[]">
                                 </div>
                             <?php endforeach; ?>
-                            <?php if (!empty($contacts)): ?>
-                                <?php foreach ($contacts as $index => $value): ?>
-                                <div>
-                                    <label for=<?=$index . "_type"?>>Tipo de contacto</label>
-                                    <input id=<?=$index . "_type"?> value='<?=$value["type"]?>' name="contact_type[]">
-                                    <label for=<?=$index . "_value"?>>Dirección de medio</label>
-                                    <input id=<?=$index . "_value"?> value='<?=$value["value"]?>' name="contact_value[]">
-                                </div>
-                                <?php endforeach; ?>
-                            <?php endif; ?>
-                        </div>
-                    </fieldset>
+                        <?php endif; ?>
+                    </div>
+                </fieldset>
 
                     <fieldset>
                         <?php if(isset($business["coverImg"])):?>
